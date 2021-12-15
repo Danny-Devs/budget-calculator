173e181855a683b037adc75f4a0926e71970696c
Daniel Ahn <et3rnal.d@gmail.com>
2021-12-14 18:48:04 -0800

    finish to ch. 21 udemy john smilga svelte

---
+++ src/ExpensesList.svelte
@@ -0,0 +1,25 @@
+<script>
+  // components
+  import SectionTitle from './Title.svelte'
+  import Expense from './Expense.svelte'
+
+  // props
+  export let expenses = []
+</script>
+
+<section>
+  <SectionTitle title="expense List" />
+  <ul>
+    {#each expenses as expense}
+      <Expense {...expense} />
+    {:else}
+      <h2>currently you have no expenses</h2>
+    {/each}
+  </ul>
+</section>
+
+<style>
+  h2 {
+    text-transform: capitalize;
+  }
+</style>
